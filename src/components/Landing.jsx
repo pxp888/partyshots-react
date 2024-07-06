4deb3ef701d55b28d1ed32a4aa27aad8315566dd
pxp888 <pxperrine@gmail.com>
2024-07-06 14:19:50 +0200

    update landing page style

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -15,11 +15,11 @@ function Landing() {
 	
 	return (
 		<div id="landing">
-			<h1>partyShots</h1>
+			<h1 className='title'>partyShots</h1>
 			<h2>because we don't all have to take the picture.</h2>
 			
 			<div className="landing1">
-				<div className="mbox lightbox">
+				<div className="hbox">
 					<h3>Get the party started</h3>
 					<p>partyShots is a photo sharing app that allows you to share photos with your friends and family. Create an album and invite your friends to join. Everyone can share their photos and videos in one place.</p>
 				</div>
@@ -29,39 +29,47 @@ function Landing() {
 			</div>
 
 			<div className="warning">
-				<h1>Nothing is private on this site</h1>
+				<h1>btw, Nothing is private on this site</h1>
 				<p>Anyone with an album code or username can find files shared on this site.</p>
 				<p>This site is intended to help people collaborate, not for personal storage.</p>
 			</div>
 
-			<div id="howitworks">
-				<h1>How it works</h1>
-			</div>
-
+			<p className='title2'>How it works</p>
+			
 			<div className="landing2">
 				<div className="imdiv2">
 					<img src={cover2} alt="landingshot" />
 				</div>
-				<div className='hbox'>
-					<div className="lightbox">
-						<h3>Create an album</h3>
-						<p>You just need to be logged in. </p>
-					</div>
-				
-					<div className="lightbox">
-						<h3>maybe add some photos</h3>
-					</div>
-					
-					<div className="lightbox">
-						<h3>share the album code, or your username</h3>
-						<p>Anyone with either the album code or your username can view the album.</p>
-						<p>Anyone logged in can see, and add files to an album.</p>
-					</div>
+				<div className="vbox">
+					<h3>Create an album</h3>
+					<p>You just need to be logged in. </p>
+					<h3>maybe add some photos,</h3>
+					<p>(it is a photo sharing site.)</p>
+					<h3>share the album code, or your username</h3>
+					<p>Anyone with either the album code or your username can view the album.</p>
+					<p>Anyone logged in can see, and add files to an album.</p>
+					<p>They can also download the album.</p>
 				</div>
 			</div>
-			
 
-			
+			<p className='title2'>albums and photos</p>
+
+			<div className="landing1">
+				<div className="hbox">
+					<h3>Uploads</h3>
+					<p>Each album and file has an owner.</p>
+					<p>Users can upload files to any album. </p>
+					<p>Users can subscribe to any album.</p>
+					<h3>deletions</h3>
+					<p>An album can only be deleted by its owner.</p>
+					<p>a file can only be removed by the owner, or the album owner.</p>
+
+
+				</div>
+				<div className="imdiv1">
+					<img src={cover5} alt="landingshot" />
+				</div>
+			</div>
 
 		</div>
 	);
--- src/components/accounts/Register.jsx
+++ src/components/accounts/Register.jsx
@@ -25,19 +25,21 @@ function Register({setCurrent}) {
 
 	return (
 		<div className="logform">
-			<h1>Register</h1>
-			<form onSubmit={handleSubmit}> 
-				<label htmlFor="name">Username</label>
-				<input type="text" name="name" placeholder="Username" />
-				<label htmlFor="email">Email</label>
-				<input type="email" name="email" placeholder="Email" />
-				<label htmlFor="password">Password</label>
-				<input type="password" name="password" placeholder="Password" />
-				<label htmlFor="password_confirmation">Confirm Password</label>
-				<input type="password" name="password_confirmation" placeholder="Confirm Password" />
-				<button type="submit">Register</button>
-			</form>
-			{message!=='' && <p>{message}</p>}
+			<div>
+				<h1>Register</h1>
+				<form onSubmit={handleSubmit}> 
+					<label htmlFor="name">Username</label>
+					<input type="text" name="name" placeholder="Username" />
+					<label htmlFor="email">Email</label>
+					<input type="email" name="email" placeholder="Email" />
+					<label htmlFor="password">Password</label>
+					<input type="password" name="password" placeholder="Password" />
+					<label htmlFor="password_confirmation">Confirm Password</label>
+					<input type="password" name="password_confirmation" placeholder="Confirm Password" />
+					<button type="submit">Register</button>
+				</form>
+				{message!=='' && <p>{message}</p>}
+			</div>
 		</div>
 	);
 }
